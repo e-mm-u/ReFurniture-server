a0db3fa78a9a01e8f93ea0c2a7c4dadf7fee7539
e-mm-u <uh.habiba1012@gmail.com>
2022-11-24 12:20:15 +0600

    mongodb connected, users get & post done

--- index.js
+++ index.js
@@ -9,7 +9,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app('/', (req,res)=>{
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.37l0ps0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const usersCollection = client.db('recycle-furniture').collection('users');
+
+        app.get('/users', async(req,res)=>{
+            const query = { };
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/users', async(req,res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+    }finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req,res)=>{
     res.send('Hello Hi bYe ByE');
 })
 
